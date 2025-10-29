ebd36e5d31e47c84fc7af698eb92e7d1ef0676ae
Alpesh <alpesh31kavankar@gmail.com>
2025-10-29 12:29:15 +0530

    new requirement.txt

--- app/services/email.py
+++ app/services/email.py
@@ -1,18 +1,3 @@
-# import smtplib
-# from email.mime.text import MIMEText
-
-# def send_activation_email(to_email: str, activation_link: str):
-#     body = f"Hello!\n\nClick this link to activate your account:\n{activation_link}\n\nThank you!"
-#     msg = MIMEText(body)
-#     msg['Subject'] = "Activate Your Account"
-#     msg['From'] = "no-reply@example.com"
-#     msg['To'] = to_email
-
-#     # Configure your SMTP server here
-#     with smtplib.SMTP("smtp.gmail.com", 587) as server:
-#         server.starttls()
-#         server.login("testsumit19@gmail.com", "dyld bnbm auks eopc")
-#         server.send_message(msg)
 
 
 import smtplib
